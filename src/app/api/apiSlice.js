7bdddba707067b97403e30908511645377023486
Andy <111096875+andyjv1@users.noreply.github.com>
2023-07-23 12:23:02 -0400

    update baseURL

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3600' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://kanban-task-management-web-app-backend.onrender.com' }),
     tagTypes: ['Board', 'Task', 'Subtask', 'Column'],
     endpoints: builder => ({})
 })
